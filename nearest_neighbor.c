47cb33e62aadab3a2812109409229de8b1034de2
DalvanOliveira <dalvan.oliveira@outlook.com>
2019-11-22 12:29:15 -0300

    final

--- nearest_neighbor.c
+++ nearest_neighbor.c
@@ -5,7 +5,6 @@
 #define INF 0x7F800000
 #include<time.h>
 
-
 int n_visited = 0;			
 long size_of_path = 0;
 char name_of_file[50];
@@ -14,33 +13,12 @@ int was_element_visited();
 int CalcDist();
 void twoOpt();
 
-void print_matrix(int s, int dim, int m[dim][dim]){
-  for(int i = 0; i < s; i++){
-    for(int j = 0; j < s; j++){
-      printf("-- %i", m[i][j]);
-    }
-    printf("\n");
-  }
-}
-
-
-
-void print_visited(int dim, int visited[dim]){
-  for(int i =0; i<=n_visited; i++){
-    printf("%i \n", visited[i]);
-  }
-}
-
-
-
 void insert_into_visited(int elem, int dim, int visited[dim]){
   if(!was_element_visited(elem)){
     visited[n_visited] = elem;
   }
+  return;
 }
-
-
-
 
 int was_element_visited(int elem, int dim, int visited[dim]){
   for(int i =0; i<=n_visited; i++){
@@ -50,20 +28,7 @@ int was_element_visited(int elem, int dim, int visited[dim]){
   return 0;
 }
 
-void imprimirMatriz(int dim, int m[dim][dim]){
-  printf("Matriz:\n");
-  for (int i = 0; i < dim; i++)
-  {
-    for (int j = 0; j < dim; j++)
-    {
-      /* code */
-      printf("-%d", m[i][j] );
-    }
-    printf("\n");
-  }
-}
-
-void construct (){
+void ConstruirMelhorar (){
     printf("Digite o nome do arquivo\n");
    scanf("%s", name_of_file);
    dimention = get_dimention(name_of_file);
@@ -72,10 +37,6 @@ void construct (){
    int visited[dimention];
    int solu[dimention];
    int tam = dimention;
-
-  //imprimirMatriz(dimention,m);
-  //printf("\n");
-
 
   for(int k = 0; k<= dimention; k++){
     visited[k] = INF;
@@ -86,11 +47,9 @@ void construct (){
   int next_i = first_i;
   int *current_v = m[next_i];
 
-
   for(int k = 0; k< dimention; k++){
     int shorter = 0x7F800000;
     int current_i = next_i;
-
 
     if(k==dimention-1){
       shorter = m[current_i][first_i];
@@ -99,7 +58,6 @@ void construct (){
     }
 
     else{
-
       for(int i = 0; i < dimention; i++){
         if(current_v[i] < shorter && current_i!=i && !was_element_visited(i, dimention, visited)){
           shorter = current_v[i];
@@ -112,20 +70,7 @@ void construct (){
     insert_into_visited(current_i, dimention, visited);
     n_visited++;
     solu[k] = current_i;
-    //printf("\nDe %i para %i com custo %i e total parcial %li",current_i,next_i, shorter, size_of_path);
 
-  }
-  //printf("\nTamanho do caminho inicial atraves de contrucao: %li\n\n", size_of_path);
-  //printf("noisSolu antes do 2opt %d \n\n",CalcDist(solu,tam,dimention,m));
-  //printf("Fase construtiva terminada. Iniando iteração melhorativa");
-
-  //clock_t tempoOrd;
-  //tempoOrd = clock();
-    //tempoOrd = clock() - tempoOrd;
- 	//printf("Tempo de execução dentro do codigo: %f \n", ((float)tempoOrd/CLOCKS_PER_SEC));
-
-
-  	////tentativa while
   	int nossaSolu=0; int nossaSoluNova=0;int comp = 0; int i=1;
   	twoOpt(solu,tam,dimention,m);
   	nossaSoluNova = CalcDist(solu,tam,dimention,m);
@@ -140,11 +85,8 @@ void construct (){
   	} while((nossaSoluNova < nossaSolu) || i == 1000);
   	printf("Iteracoes / Inicial / Final: %d / %li / %d\n",i, size_of_path,nossaSoluNova );
  return;   
+  }
 }
-
-
-
-//2opt começa aqui ---------
 
 int conflito(int *solu,int i,int j,int tam){
     if (i == j ){
@@ -169,27 +111,18 @@ int troca(int *solu,int i,int j,int tam){
 int CalcDist2(int *solu,int tam,int dim, int m[dim][dim]){
     int total = 0;
     for (int i = 0; i < (tam-1); i++){
-    	total += m[solu[i]-1] [solu[i+1]-1];
-    //	printf("de %d para %d eh %d\n",solu[i],solu[i+1], m[solu[i]-1] [solu[i+1]-1]);
-    //	printf("total %d\n",total );
-        
+    	total += m[solu[i]-1] [solu[i+1]-1]; 
     }
     total += m[solu[tam-1]-1][((solu[0]))-1];
-    //printf("de %d para %d eh: %d\n",(solu[tam-1]),((solu[0])), m[solu[tam-1]-1][((solu[0]))-1]);
-   // printf("Soma: %d\n", total);
     return total;
-
 }
 
 int CalcDist(int *solu,int tam,int dim, int m[dim][dim]){
     int total = 0;
     for (int i = 0; i < (tam-1); i++){
       total += m[solu[i]] [solu[i+1]];
-      //printf("vetor valor do vetor solu na posicao %d eh %d eh linha %d\n",i,solu[i], solu[i]);
-      //printf("de %d para %d com custo %d e total partial %d\n",solu[i],solu[i+1], m[solu[i]] [solu[i+1]], total);
     }
     total += m[solu[tam-1]][((solu[0]))];
-    //printf("de %d para %d com custo %d e total partial %d\n",(solu[tam-1]),((solu[0])), m[solu[tam-1]][((solu[0]))],total);
     return total;
 }
 
@@ -232,15 +165,8 @@ void twoOpt(int *solu, int tam, int dimention, int m[dimention][dimention]){
         return;
 }
 
-
-//-------------------
-
-
 int main()
 {
-  construct();
-
-  
-
+  ConstruirMelhorar();
   return 0;
 }
